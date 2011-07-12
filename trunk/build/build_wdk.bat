389138a677dc6ded6bf05e0f9a047430a9f3e56a
XhmikosR <XhmikosR@00890fb1-b31f-75c6-3bde-20cb27c6bd18>
2011-07-12 14:51:00 +0000

    * rename "build_wdk" folder to "build" * rename "build.cmd" to "build_wdk.bat" * WDK builds: put each projects object files in their respective subdirectories * VS2010 builds: do the same as the WDK builds regarding the object files, and also clean up the projects

--- trunk/build/build_wdk.bat
+++ trunk/build/build_wdk.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 rem *
-rem * build.cmd, Batch file "wrapper" for makefile.mak, 
+rem * build.bat, Batch file "wrapper" for makefile.mak, 
 rem * used to build CoolPlayer with WDK
 rem *
 rem * Copyright (C) 2010-2011 XhmikosR
@@ -20,7 +20,7 @@ rem * along with this program; if not, write to the Free Software
 rem * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
-SETLOCAL
+SETLOCAL ENABLEEXTENSIONS
 CD /D %~dp0
 
 rem Set the WDK directory
@@ -29,37 +29,14 @@ SET "WDKBASEDIR=C:\WinDDK\7600.16385.1"
 rem Check the building environment
 IF NOT EXIST "%WDKBASEDIR%" CALL :SUBMSG "ERROR" "Specify your WDK directory!"
 
-rem check for the help switches
-IF /I "%1"=="help"   GOTO SHOWHELP
-IF /I "%1"=="/help"  GOTO SHOWHELP
-IF /I "%1"=="-help"  GOTO SHOWHELP
-IF /I "%1"=="--help" GOTO SHOWHELP
-IF /I "%1"=="/?"     GOTO SHOWHELP
-GOTO CHECKFIRSTARG
+rem Check for the help switches
+IF /I "%~1"=="help"   GOTO SHOWHELP
+IF /I "%~1"=="/help"  GOTO SHOWHELP
+IF /I "%~1"=="-help"  GOTO SHOWHELP
+IF /I "%~1"=="--help" GOTO SHOWHELP
+IF /I "%~1"=="/?"     GOTO SHOWHELP
 
 
-:SHOWHELP
-TITLE "%~nx0 %1"
-ECHO.
-ECHO Usage:  %~nx0 [Clean^|Build^|Rebuild] [x86^|x64^|all]
-ECHO.
-ECHO Note:   You can also prefix the commands with "-", "--" or "/".
-ECHO.
-ECHO.
-ECHO Edit "%~nx0" and set your WDK directory.
-ECHO You shouldn't need to make any changes other than that.
-ECHO.
-ECHO.
-ECHO Executing "%~nx0" will use the defaults: "%~nx0 build all"
-ECHO If you skip the second argument the default one will be used. Example:
-ECHO "%~nx0 rebuild" is equivalent to "%~nx0 rebuild all"
-ECHO NOTE: "%~nx0 x86" won't work.
-ECHO.
-ENDLOCAL
-EXIT /B
-
-
-:CHECKFIRSTARG
 rem Check for the first switch
 IF "%~1" == "" (
   SET "BUILDTYPE=Build"
@@ -89,18 +66,18 @@ rem Check for the second switch
 IF "%~2" == "" (
   SET "ARCH=all"
 ) ELSE (
-  IF /I "%~2" == "x86"   SET "ARCH=x86" & GOTO x86
-  IF /I "%~2" == "/x86"  SET "ARCH=x86" & GOTO x86
-  IF /I "%~2" == "-x86"  SET "ARCH=x86" & GOTO x86
-  IF /I "%~2" == "--x86" SET "ARCH=x86" & GOTO x86
-  IF /I "%~2" == "x64"   SET "ARCH=x64" & GOTO x86
-  IF /I "%~2" == "/x64"  SET "ARCH=x64" & GOTO x86
-  IF /I "%~2" == "-x64"  SET "ARCH=x64" & GOTO x86
-  IF /I "%~2" == "--x64" SET "ARCH=x64" & GOTO x86
-  IF /I "%~2" == "all"   SET "ARCH=all" & GOTO x86
-  IF /I "%~2" == "/all"  SET "ARCH=all" & GOTO x86
-  IF /I "%~2" == "-all"  SET "ARCH=all" & GOTO x86
-  IF /I "%~2" == "--all" SET "ARCH=all" & GOTO x86
+  IF /I "%~2" == "x86"   SET "ARCH=x86" & GOTO START
+  IF /I "%~2" == "/x86"  SET "ARCH=x86" & GOTO START
+  IF /I "%~2" == "-x86"  SET "ARCH=x86" & GOTO START
+  IF /I "%~2" == "--x86" SET "ARCH=x86" & GOTO START
+  IF /I "%~2" == "x64"   SET "ARCH=x64" & GOTO START
+  IF /I "%~2" == "/x64"  SET "ARCH=x64" & GOTO START
+  IF /I "%~2" == "-x64"  SET "ARCH=x64" & GOTO START
+  IF /I "%~2" == "--x64" SET "ARCH=x64" & GOTO START
+  IF /I "%~2" == "all"   SET "ARCH=all" & GOTO START
+  IF /I "%~2" == "/all"  SET "ARCH=all" & GOTO START
+  IF /I "%~2" == "-all"  SET "ARCH=all" & GOTO START
+  IF /I "%~2" == "--all" SET "ARCH=all" & GOTO START
 
   ECHO.
   ECHO Unsupported commandline switch!
@@ -109,53 +86,54 @@ IF "%~2" == "" (
 )
 
 
-:x86
-SET "INCLUDE=%WDKBASEDIR%\inc\crt;%WDKBASEDIR%\inc\api;%WDKBASEDIR%\inc\api\crt\stl60;%WDKBASEDIR%\inc\ddk"
+:START
+SET "INCLUDE=%WDKBASEDIR%\inc\api;%WDKBASEDIR%\inc\api\crt\stl60;%WDKBASEDIR%\inc\crt;%WDKBASEDIR%\inc\ddk"
 SET "LIB=%WDKBASEDIR%\lib\crt\i386;%WDKBASEDIR%\lib\win7\i386"
 SET "PATH=%WDKBASEDIR%\bin\x86;%WDKBASEDIR%\bin\x86\x86"
 
-IF /I "%ARCH%" == "x64" GOTO x64
+IF "%ARCH%" == "x64" GOTO x64
 
-TITLE Building CoolPlayer x86...
-ECHO. && ECHO.
+TITLE Building CoolPlayer x86 with WDK...
+ECHO. & ECHO.
 
-IF /I "%BUILDTYPE%" == "Build" (
+IF "%BUILDTYPE%" == "Build" (
   CALL :SUBNMAKE
 
-  IF /I "%ARCH%" == "x86" GOTO END
-  IF /I "%ARCH%" == "x64" GOTO x64
-  IF /I "%ARCH%" == "all" GOTO x64
+  IF "%ARCH%" == "x86" GOTO END
+  IF "%ARCH%" == "x64" GOTO x64
+  IF "%ARCH%" == "all" GOTO x64
 )
 
-IF /I "%BUILDTYPE%" == "Rebuild" (
+IF "%BUILDTYPE%" == "Rebuild" (
   CALL :SUBNMAKE
 
-  IF /I "%ARCH%" == "x86" GOTO END
-  IF /I "%ARCH%" == "x64" GOTO x64
-  IF /I "%ARCH%" == "all" GOTO x64
+  IF "%ARCH%" == "x86" GOTO END
+  IF "%ARCH%" == "x64" GOTO x64
+  IF "%ARCH%" == "all" GOTO x64
 )
 
-IF /I "%BUILDTYPE%" == "Clean" CALL :SUBNMAKE clean
-IF /I "%ARCH%" == "x86" GOTO END
-IF /I "%ARCH%" == "x64" GOTO x64
-IF /I "%ARCH%" == "all" GOTO x64
+IF "%BUILDTYPE%" == "Clean" CALL :SUBNMAKE
+
+IF "%ARCH%" == "x86" GOTO END
+IF "%ARCH%" == "x64" GOTO x64
+IF "%ARCH%" == "all" GOTO x64
 
 
 :x64
 SET "LIB=%WDKBASEDIR%\lib\crt\amd64;%WDKBASEDIR%\lib\win7\amd64"
 SET "PATH=%WDKBASEDIR%\bin\x86;%WDKBASEDIR%\bin\x86\amd64"
 
-IF /I "%ARCH%" == "x86" GOTO END
+IF "%ARCH%" == "x86" GOTO END
 
-TITLE Building CoolPlayer x64...
-ECHO. && ECHO.
+TITLE Building CoolPlayer x64 with WDK...
+ECHO. & ECHO.
 
-IF /I "%BUILDTYPE%" == "Build" (
+IF "%BUILDTYPE%" == "Build" (
   CALL :SUBNMAKE "x64=1"
   GOTO END
 )
 
-IF /I "%BUILDTYPE%" == "Rebuild" (
+IF "%BUILDTYPE%" == "Rebuild" (
   CALL :SUBNMAKE "x64=1"
   GOTO END
 )
@@ -164,7 +142,7 @@ IF /I "%BUILDTYPE%" == "Clean" CALL :SUBNMAKE "x64=1" clean
 
 
 :END
-TITLE Building CoolPlayer - Finished!
+TITLE Building CoolPlayer with WDK - Finished!
 ENDLOCAL
 EXIT /B
 
@@ -172,6 +150,28 @@ EXIT /B
 :SUBNMAKE
 nmake /NOLOGO /f "makefile.mak" %BUILDTYPE% %1
 IF %ERRORLEVEL% NEQ 0 CALL :SUBMSG "ERROR" "Compilation failed!"
+EXIT /B
+
+
+:SHOWHELP
+TITLE "%~nx0 %1"
+ECHO. & ECHO.
+ECHO Usage:   %~nx0 [Clean^|Build^|Rebuild] [x86^|x64^|all]
+ECHO.
+ECHO Notes:   You can also prefix the commands with "-", "--" or "/".
+ECHO          The arguments are not case sensitive.
+ECHO. & ECHO.
+ECHO Edit "%~nx0" and set your WDK directory.
+ECHO You shouldn't need to make any changes other than that.
+ECHO. & ECHO.
+ECHO Executing "%~nx0" will use the defaults: "%~nx0 build all"
+ECHO.
+ECHO If you skip the second argument the default one will be used. Example:
+ECHO "%~nx0 rebuild" is the same as "%~nx0 rebuild all"
+ECHO.
+ECHO WARNING: "%~nx0 x86" won't work.
+ECHO.
+ENDLOCAL
 EXIT /B
 
 
